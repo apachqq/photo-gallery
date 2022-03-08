53838f7c1c1d15375d588710af1cb7d0a739b23d
apachqq <work.tsyganov@mail.ru>
2022-03-08 03:29:10 +0300

    second variables document.querySelector

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ let pictures = [
     'img/tomato-yellow-large.png',
     'img/tomato-strange-large.png'
 ]
+
+let thumbnails = document.querySelectorAll('.gallery__picture-preview')
+let fullPhoto = document.querySelector('.full-picture')
